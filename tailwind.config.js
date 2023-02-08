7ccf0bedf4c916af45f0278196a4bcfa280956e1
mlhoutel <mlhoutel@gmail.com>
2023-02-08 11:43:24 +0100

    feat: :art: Launcher view
    Add global neumorphism style and prepare launch view popup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Nunito Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
