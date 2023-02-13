0a2f00f62edbc35e59e7771b038122b2377350a7
mlhoutel <mlhoutel@gmail.com>
2023-02-13 14:37:32 +0100

    feat: :zap: Add a minio client and move api in src folder
    Add env var and a utility function to upload several files to a s3 bucket

---
+++ src/clients/minio/index.ts
@@ -0,0 +1,36 @@
+var Minio = require('minio')
+require('dotenv').config()
+
+// Instantiate the minio client with the endpoint
+const minioClient = new Minio.Client({
+    endPoint: process.env['S3_BUCKET_END_POINT'],
+    port: process.env['S3_BUCKET_PORT'],
+    useSSL: true,
+    accessKey: process.env['S3_BUCKET_ACCESS_KEY'],
+    secretKey: process.env['S3_BUCKET_SECRET_KEY']
+});
+
+function uploadFilesToS3(bucketName: string, files: string[]): void {
+
+  minioClient.makeBucket(bucketName, 'eu-west-1', function(e: Error) {
+    if (e) {
+      throw new Error("Exception while creating the s3 bucket: " + e)
+    }
+
+    const metaData = {
+        'Content-Type': 'application/octet-stream',
+        'X-Amz-Meta-Testing': 1234,
+        'example': 5678
+    }
+
+    for (const file of files) {
+      minioClient.fPutObject(bucketName, 'photos-europe.tar', file, metaData, function(e: Error, etag: any) {
+        if (e) {
+          throw new Error("Exception while uplkoading file to the s3 bucket: " + e)
+        }
+      });
+    }
+  });
+}
+
+export { minioClient, uploadFilesToS3 }
